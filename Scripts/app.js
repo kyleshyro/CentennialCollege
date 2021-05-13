d409a89b558ae3d856a8db9a48656dd304bb6a1f
Rajaofera Tiana <65467815+kyleshyro@users.noreply.github.com>
2021-05-13 00:52:41 -0400

    files uploaded

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+// IIFE -- Immediately Invoked Function Expression
+"use strict";
+(function(){
+
+    function Start(){
+        let myMessage = 10;
+    let i;
+    console.log(`my number is :  ${myMessage}`);
+   
+    }
+    window.addEventListener("load", Start);
+
+})()
+
+
+
+
